e7482b8ecf77c2780294657632a118679eb68978
Milthril <Milthril@users.noreply.github.com>
2017-12-27 11:27:15 +0800

    修正ppc参考代码bug

--- Book/pcc/chapter_16/world_population.py
+++ Book/pcc/chapter_16/world_population.py
@@ -2,7 +2,7 @@ import json
 
 import pygal
 from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
-
+import pygal.maps.world
 from country_codes import get_country_code
 
 # Load the data into a list.
@@ -34,7 +34,7 @@ for cc, pop in cc_populations.items():
 print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
 
 wm_style = RS('#336699', base_style=LCS)
-wm = pygal.Worldmap(style=wm_style)
+wm = pygal.maps.world.World()
 wm.title = 'World Population in 2010, by Country'
 wm.add('0-10m', cc_pops_1)
 wm.add('10m-1bn', cc_pops_2)
